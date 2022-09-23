3c9fdff017d18b9aa0aee896846521a616847378
matheus ribeiro santana <802.243@alunos.unigran.br>
2022-09-23 19:21:50 -0400

    Criando entidade abastecimento

---
+++ app/src/main/java/br/unigran/p1/Entidades/abastecimento.java
@@ -0,0 +1,60 @@
+package br.unigran.p1.Entidades;
+
+public class abastecimento {
+    private String kmAtual;
+    private String qtdAbastecida;
+    private String dia;
+    private String valor;
+    private Integer id;
+
+    public String getKmAtual() {
+        return kmAtual;
+    }
+
+    public void setKmAtual(String kmAtual) {
+        this.kmAtual = kmAtual;
+    }
+
+    public String getQtdAbastecida() {
+        return qtdAbastecida;
+    }
+
+    public void setQtdAbastecida(String qtdAbastecida) {
+        this.qtdAbastecida = qtdAbastecida;
+    }
+
+    public String getDia() {
+        return dia;
+    }
+
+    public void setDia(String dia) {
+        this.dia = dia;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "abastecimento{" +
+                "kmAtual='" + kmAtual + '\'' +
+                ", qtdAbastecida='" + qtdAbastecida + '\'' +
+                ", dia='" + dia + '\'' +
+                ", valor='" + valor + '\'' +
+                ", id=" + id +
+                '}';
+    }
+}
